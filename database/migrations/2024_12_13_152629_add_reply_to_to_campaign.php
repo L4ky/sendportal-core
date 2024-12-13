445ed497761869fd18d56b905f4a416d7d8bb590
Luca S. <luca.salvestrini@rozro.com>
2024-12-13 17:20:37 +0100

    Add reply_to to campaigns table

---
+++ database/migrations/2024_12_13_152629_add_reply_to_to_campaign.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('sendportal_campaigns', function (Blueprint $table) {
+            $table->string('reply_to')->nullable()->after('from_email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('sendportal_campaigns', function (Blueprint $table) {
+            $table->dropColumn('reply_to');
+        });
+    }
+};
